74022fae5b0740971c014d7325820a8c48944cb0
sjoseph-22 <officialsjo22@gmail.com>
2025-10-29 15:24:31 +0100

    additional setup files

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+
+def error_message_details(error, error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    
+    error_message = f"Error occured in python file: {filename}, line number: {exc_tb.tb_lineno}. Error Message: {str(error)}"
+    
+    return error_message
+
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_details=error_details)
+        
+    def __str__(self):
+        return self.error_message
+    
+    
